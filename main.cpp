121ad8de8c8d5f123489d58c76c25236d1eb57ef
linken <hud_tom@163.com>
2020-04-19 10:44:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main(int argc, char **argv)
 	
 	cout<<"initSuccess:"<<ec::initSuccess(0)<<endl;
 	cout<<"initFail:"<<ec::initFail(0)<<endl;
+	
 	rt = ec::setModuleNum(0, 1);
 	cout<<"setModuleNum:8:rt:"<<rt<<":getModuleNum:"<<ec::getModuleNum(rt)<<endl;
+	
 	rt = ec::setErrNum(rt, 1);
 	cout<<"setErrNum:6:rt:"<<rt<<":getErrNum:"<<ec::getErrNum(rt)<<endl;
 	cout<<"err string "<<ec::Desc(rt);
